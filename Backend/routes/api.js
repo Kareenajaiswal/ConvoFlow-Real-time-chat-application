ef4ab7833970efd80d1fbd6a62371339d00dea64
prashant prasad <prashant.prasad@gdcl.in>
2024-09-02 16:54:03 +0530

    updated api of messages

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -4,7 +4,7 @@ const authCheck = require("../middleware/authCheck");
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../inc/config");
 const { tokenBlacklist } = require("../inc/config");
-const { User, Chat } = require("../inc/db");
+const { User, Chat, Message } = require("../inc/db");
 
 
 // LOGIN & REGISTRATION
@@ -80,7 +80,7 @@ router.get("/profile", authCheck, async (req, res) => {
     const token = req.headers.authorization;
     const tokenDecode = jwt.decode(token);
     const userDetails = await User.findOne({
-        _id : tokenDecode.id
+        _id: tokenDecode.id
     })
 
     res.json({
@@ -90,12 +90,13 @@ router.get("/profile", authCheck, async (req, res) => {
 })
 
 
-// CHATS
+// CHATS LIST (One to One)
+
 router.get("/chats", authCheck, async (req, res) => {
     const token = req.headers.authorization;
     const tokenDecode = jwt.decode(token);
-    const ChatList = await Chat.findOne({
-        "participants.0" : tokenDecode.id
+    const ChatList = await Chat.find({
+        participants: { $in: [tokenDecode.id] }  // sender and receiver both will have the same chat list, only if there are common chat room
     })
 
     res.json({
@@ -112,13 +113,76 @@ router.post("/chats", authCheck, async (req, res) => {
     const receiverid = req.body.receiverid;
 
     const chatCreate = await Chat.create({
-        participants : [id,receiverid]
+        participants: [id, receiverid]
     })
 
     res.json({
-        msg : "Chat Created !!",
-        chatID : chatCreate._id
+        msg: "Chat Created !!",
+        chatID: chatCreate._id
     })
 })
+
+//CHAT ROOM (One to One)
+
+router.get("/chats/messages", authCheck, async (req, res) => { //Inprocess
+    const token = req.headers.authorization;
+    const tokenDecode = jwt.decode(token);
+    const chatId = req.body.chatId; // Which specific chat history you want to see
+
+    //check for the user wether it is a participants is in chat participants array or not,if not then don't proceed
+
+    const chatValidate = await Chat.findOne({
+        _id : chatId,
+        participants: { $in: [tokenDecode.id] }
+    })
+
+    if(chatValidate){
+        const chatRoom = await Message.find({
+            chatId
+        })
+    
+        res.json({
+            chatRoom
+        })
+    }else{
+        res.json({
+            msg : null
+        })
+    }
+
+    
+
+})
+
+router.post("/chats/messages", authCheck, async (req, res) => { //Working
+    const token = req.headers.authorization;
+    const tokenDecode = jwt.decode(token);
+    const id = tokenDecode.id;
+    const chatId = req.body.chatId; //where do you want to send your message
+    const content = req.body.content; //what is the content of your message
+
+    const message = await Message.create({
+        chatId,
+        sender: id,
+        content
+    })
+
+    const chatUpdate = await Chat.updateOne({
+        _id: chatId
+    }, {
+        lastMessage: content,
+        lastMessageTime: message.timestamp,
+    })
+
+    res.json({
+        chat_details: chatUpdate,
+        msg: "Message sent successfully !!"
+    })
+
+
+
+
+})
+
 
 module.exports = router, tokenBlacklist;
