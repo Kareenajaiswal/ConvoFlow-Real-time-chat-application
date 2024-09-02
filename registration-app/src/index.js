789e5b8fe937b669129dd5f86c9fcb2fdfc76560
kareena jaiswal <Kareenajaiswal@users.noreply.github.com>
2024-09-02 22:05:04 +0530

    updated something

---
+++ registration-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Comment out or remove the following lines if not needed
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Comment out or remove the following line if not needed
+// reportWebVitals();
