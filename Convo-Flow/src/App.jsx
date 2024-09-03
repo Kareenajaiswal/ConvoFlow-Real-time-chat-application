ca1b49cc1f2ee2c1f33a776b3deea81dc366bac1
kareena jaiswal <Kareenajaiswal@users.noreply.github.com>
2024-09-03 22:40:39 +0530

    modified json file

--- Convo-Flow/src/App.jsx
+++ Convo-Flow/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './login'; 
 import Register from './register';
 import Chat from "./components/Chat";
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Register />} />
           //socket testing are
           <Route path="/chat/:chatId" element={<Chat />} />
+          <Route path="/UserProfile" element={<UserProfile />} />
         </Routes>
       </div>
     </Router>
